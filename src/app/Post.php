c5457df4db739b8ed00846ea7180b6df1fb03a8a
takuya1003 <takuya1003@users.noreply.github.com>
2020-12-14 16:00:48 +0900

    トップページ追加

--- src/app/Post.php
+++ src/app/Post.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    
+    protected $fillable = [
+        'user_id',
+        'prefectures_id',
+        'content',
+        'facility_name',
+
+    ];
+
+    public function prefecture()
+    {
+        return $this->belongsTo('App\Prefecture', 'prefectures_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
 }
