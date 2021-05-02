01970f8c896a505c3f4ea274ae3aaeb2099f12ff
takuya1003 <takuya1003@users.noreply.github.com>
2021-05-02 17:37:17 +0900

    マイグレション実行、seeder準備

---
+++ src/database/seeds/PortTableSeeder.php
@@ -0,0 +1,139 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PortTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //漁港情報
+        DB::table('port')->insert([
+            [
+                'id' => 1,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 2,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 3,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 4,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 5,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 6,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 7,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 8,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 9,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 10,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 11,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 12,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 13,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 14,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 15,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 16,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 17,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 18,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 19,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+            [
+                'id' => 20,
+                'port_name' => '',
+                'latitude' => '',
+                'longitude' => ''
+            ],
+
+        ]);
+    }
+}
